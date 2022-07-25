9dfbf6c3faa79dfc9e8e9ad060f120a19627d5df
liyitongxue <48759692+liyitongxue@users.noreply.github.com>
2022-07-25 18:01:13 +0800

    vuepress-theme-hope主题博客
    vuepress-theme-hope主题博客

---
+++ docs/.vuepress/.temp/pages/note/client/1.html.js
@@ -0,0 +1,14 @@
+export const data = JSON.parse("{\"key\":\"v-1c197530\",\"path\":\"/note/client/1.html\",\"title\":\"Vue.js安装与创建默认项目（详细步骤）\",\"lang\":\"zh-CN\",\"frontmatter\":{\"title\":\"Vue.js安装与创建默认项目（详细步骤）\",\"shortTitle\":null,\"description\":null,\"icon\":\"vue\",\"order\":1,\"author\":null,\"isOriginal\":true,\"date\":\"2022-01-12T00:00:00.000Z\",\"category\":[\"client\"],\"tag\":[\"Vue.js\"],\"sticky\":null,\"star\":true,\"article\":null,\"timeline\":null,\"image\":null,\"banner\":null,\"footer\":\"\",\"copyright\":\"Copyright © 2022 LIYITONGXUE\",\"summary\":\"Vue是一套用于构建用户界面的渐进式JavaScript框架。与其它大型框架不同的是，Vue 被设计为可以自底向上逐层应用。Vue 的核心库只关注视图层，不仅易于上手，还便于与第三方库或既有项目整合。另一方面，当与现代化的工具链以及各种支持类库结合使用时，Vue 也完全能够为复杂的单页应用（SPA）提供驱动。\\n\",\"head\":[[\"meta\",{\"property\":\"og:url\",\"content\":\"http://www.liyi.io/note/client/1.html\"}],[\"meta\",{\"property\":\"og:site_name\",\"content\":\"LIYITONGXUE\"}],[\"meta\",{\"property\":\"og:title\",\"content\":\"Vue.js安装与创建默认项目（详细步骤）\"}],[\"meta\",{\"property\":\"og:type\",\"content\":\"article\"}],[\"meta\",{\"property\":\"og:locale\",\"content\":\"zh-CN\"}],[\"meta\",{\"property\":\"og:locale:alternate\",\"content\":\"en-US\"}],[\"meta\",{\"property\":\"article:tag\",\"content\":\"Vue.js\"}],[\"meta\",{\"property\":\"article:published_time\",\"content\":\"2022-01-12T00:00:00.000Z\"}]]},\"excerpt\":\"<p>Vue是一套用于构建用户界面的渐进式JavaScript框架。与其它大型框架不同的是，Vue 被设计为可以自底向上逐层应用。Vue 的核心库只关注视图层，不仅易于上手，还便于与第三方库或既有项目整合。另一方面，当与现代化的工具链以及各种支持类库结合使用时，Vue 也完全能够为复杂的单页应用（SPA）提供驱动。</p>\\n\",\"headers\":[{\"level\":2,\"title\":\"1. 直接用 <script>标签引入\",\"slug\":\"_1-直接用-script-标签引入\",\"children\":[]},{\"level\":2,\"title\":\"2. 通过NPM安装Vue\",\"slug\":\"_2-通过npm安装vue\",\"children\":[]},{\"level\":2,\"title\":\"1.安装webpack\",\"slug\":\"_1-安装webpack\",\"children\":[]},{\"level\":2,\"title\":\"2.安装webpack-cli\",\"slug\":\"_2-安装webpack-cli\",\"children\":[]},{\"level\":2,\"title\":\"3.检查是否安装成功\",\"slug\":\"_3-检查是否安装成功\",\"children\":[]},{\"level\":2,\"title\":\"1.创建项目\",\"slug\":\"_1-创建项目\",\"children\":[]},{\"level\":2,\"title\":\"2.启动项目\",\"slug\":\"_2-启动项目\",\"children\":[]}],\"readingTime\":{\"minutes\":4.72,\"words\":1417},\"filePathRelative\":\"note/client/1.md\",\"localizedDate\":\"2022年1月12日\"}")
+
+if (import.meta.webpackHot) {
+  import.meta.webpackHot.accept()
+  if (__VUE_HMR_RUNTIME__.updatePageData) {
+    __VUE_HMR_RUNTIME__.updatePageData(data)
+  }
+}
+
+if (import.meta.hot) {
+  import.meta.hot.accept(({ data }) => {
+    __VUE_HMR_RUNTIME__.updatePageData(data)
+  })
+}
---
+++ docs/perception/README.md
@@ -0,0 +1,16 @@
+---
+
+icon: write
+title: 所思所想
+date: 2022-07-21
+article: false
+
+index: false
+prev: true
+next: true
+---
+
+
+
+己所不欲勿施于人！<br />
+己所欲勿施于人！
