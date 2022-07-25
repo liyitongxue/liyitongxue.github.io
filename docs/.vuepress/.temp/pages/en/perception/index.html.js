de1d5437f3fff356bcbac7940592e5facb011938
liyitongxue <48759692+liyitongxue@users.noreply.github.com>
2022-07-25 18:14:32 +0800

    1

--- docs/.vuepress/.temp/pages/en/perception/index.html.js
+++ docs/.vuepress/.temp/pages/en/perception/index.html.js
@@ -1,14 +1,1 @@
-export const data = JSON.parse("{\"key\":\"v-d1e35a06\",\"path\":\"/en/perception/\",\"title\":\"Life Perception\",\"lang\":\"en-US\",\"frontmatter\":{\"icon\":\"write\",\"title\":\"Life Perception\",\"date\":\"2022-07-21T00:00:00.000Z\",\"article\":false,\"index\":false,\"prev\":true,\"next\":true,\"summary\":\"Don't do to others what you don't want to be done to you. Don't do to others what you want to be done to you.\",\"head\":[[\"meta\",{\"property\":\"og:url\",\"content\":\"http://www.liyi.io/en/perception/\"}],[\"meta\",{\"property\":\"og:site_name\",\"content\":\"LIYITONGXUE\"}],[\"meta\",{\"property\":\"og:title\",\"content\":\"Life Perception\"}],[\"meta\",{\"property\":\"og:type\",\"content\":\"article\"}],[\"meta\",{\"property\":\"og:locale\",\"content\":\"en-US\"}],[\"meta\",{\"property\":\"og:locale:alternate\",\"content\":\"zh-CN\"}],[\"meta\",{\"property\":\"article:published_time\",\"content\":\"2022-07-21T00:00:00.000Z\"}]]},\"excerpt\":\"\",\"headers\":[],\"readingTime\":{\"minutes\":0.15,\"words\":46},\"filePathRelative\":\"en/perception/README.md\",\"localizedDate\":\"July 21, 2022\"}")
-
-if (import.meta.webpackHot) {
-  import.meta.webpackHot.accept()
-  if (__VUE_HMR_RUNTIME__.updatePageData) {
-    __VUE_HMR_RUNTIME__.updatePageData(data)
-  }
-}
-
-if (import.meta.hot) {
-  import.meta.hot.accept(({ data }) => {
-    __VUE_HMR_RUNTIME__.updatePageData(data)
-  })
-}
+export const data = JSON.parse("{\"key\":\"v-d1e35a06\",\"path\":\"/en/perception/\",\"title\":\"Life Perception\",\"lang\":\"en-US\",\"frontmatter\":{\"icon\":\"write\",\"title\":\"Life Perception\",\"date\":\"2022-07-21T00:00:00.000Z\",\"article\":false,\"index\":false,\"prev\":true,\"next\":true,\"summary\":\"Don't do to others what you don't want to be done to you. Don't do to others what you want to be done to you.\",\"head\":[[\"meta\",{\"property\":\"og:url\",\"content\":\"http://www.liyi.io/en/perception/\"}],[\"meta\",{\"property\":\"og:site_name\",\"content\":\"LIYITONGXUE\"}],[\"meta\",{\"property\":\"og:title\",\"content\":\"Life Perception\"}],[\"meta\",{\"property\":\"og:type\",\"content\":\"article\"}],[\"meta\",{\"property\":\"og:locale\",\"content\":\"en-US\"}],[\"meta\",{\"property\":\"og:locale:alternate\",\"content\":\"zh-CN\"}],[\"meta\",{\"property\":\"article:published_time\",\"content\":\"2022-07-21T00:00:00.000Z\"}]]},\"excerpt\":\"\",\"headers\":[],\"git\":{},\"readingTime\":{\"minutes\":0.15,\"words\":46},\"filePathRelative\":\"en/perception/README.md\",\"localizedDate\":\"July 21, 2022\"}")
--- docs/.vuepress/.temp/pages/intro.html.js
+++ docs/.vuepress/.temp/pages/intro.html.js
@@ -1,14 +1,1 @@
-export const data = JSON.parse("{\"key\":\"v-184f4da6\",\"path\":\"/intro.html\",\"title\":\"个人介绍\",\"lang\":\"zh-CN\",\"frontmatter\":{\"icon\":\"info\",\"article\":false,\"summary\":\"个人介绍 小单位普通员工一枚\",\"head\":[[\"meta\",{\"property\":\"og:url\",\"content\":\"http://www.liyi.io/intro.html\"}],[\"meta\",{\"property\":\"og:site_name\",\"content\":\"LIYITONGXUE\"}],[\"meta\",{\"property\":\"og:title\",\"content\":\"个人介绍\"}],[\"meta\",{\"property\":\"og:type\",\"content\":\"article\"}],[\"meta\",{\"property\":\"og:locale\",\"content\":\"zh-CN\"}],[\"meta\",{\"property\":\"og:locale:alternate\",\"content\":\"en-US\"}]]},\"excerpt\":\"\",\"headers\":[],\"readingTime\":{\"minutes\":0.06,\"words\":17},\"filePathRelative\":\"intro.md\"}")
-
-if (import.meta.webpackHot) {
-  import.meta.webpackHot.accept()
-  if (__VUE_HMR_RUNTIME__.updatePageData) {
-    __VUE_HMR_RUNTIME__.updatePageData(data)
-  }
-}
-
-if (import.meta.hot) {
-  import.meta.hot.accept(({ data }) => {
-    __VUE_HMR_RUNTIME__.updatePageData(data)
-  })
-}
+export const data = JSON.parse("{\"key\":\"v-184f4da6\",\"path\":\"/intro.html\",\"title\":\"个人介绍\",\"lang\":\"zh-CN\",\"frontmatter\":{\"icon\":\"info\",\"article\":false,\"summary\":\"个人介绍 小单位普通员工一枚\",\"head\":[[\"meta\",{\"property\":\"og:url\",\"content\":\"http://www.liyi.io/intro.html\"}],[\"meta\",{\"property\":\"og:site_name\",\"content\":\"LIYITONGXUE\"}],[\"meta\",{\"property\":\"og:title\",\"content\":\"个人介绍\"}],[\"meta\",{\"property\":\"og:type\",\"content\":\"article\"}],[\"meta\",{\"property\":\"og:locale\",\"content\":\"zh-CN\"}],[\"meta\",{\"property\":\"og:locale:alternate\",\"content\":\"en-US\"}]]},\"excerpt\":\"\",\"headers\":[],\"git\":{},\"readingTime\":{\"minutes\":0.06,\"words\":17},\"filePathRelative\":\"intro.md\"}")
